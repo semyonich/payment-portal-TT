dc6e2d42a4a7cbd77b9af885e95d2ab1f60f0f60
Serhii Semenykhin <semyonich@users.noreply.github.com>
2021-03-02 10:34:12 +0200

    initial commit

---
+++ src/main/java/com/testtask/demo/PaymentPortalTtApplication.java
@@ -0,0 +1,13 @@
+package com.testtask.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentPortalTtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentPortalTtApplication.class, args);
+    }
+
+}
